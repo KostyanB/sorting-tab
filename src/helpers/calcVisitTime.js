8729e8445f1a489f658be6e97e2eb30ed3b91523
Konstantin Berezko <berezkok72@gmail.com>
2021-11-20 10:57:22 +0500

    fix(sortingTab):fix calc visitTime

--- src/helpers/calcVisitTime.js
+++ src/helpers/calcVisitTime.js
@@ -1,13 +1,9 @@
 import toTwoDigit from './toTwoDigit';
 
 const createVisitTimeText = visitTime => {
-    if (visitTime > 0) {
-        const visitMinute = toTwoDigit(Math.floor(visitTime % 60));
-        const visitHour = toTwoDigit(Math.floor(visitTime / 60));
-        return `${visitHour}:${visitMinute}`;
-    } else {
-        return '0';
-    }
+    const visitMinute = toTwoDigit(Math.floor(visitTime % 60));
+    const visitHour = toTwoDigit(Math.floor(visitTime / 60));
+    return `${visitHour}:${visitMinute}`;
 };
 
 const getTime = (date, time) => new Date(`${date} ${time.split('-').join(':')}`);
